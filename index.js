0932021d83bdc76d6e88b18f1ab784b8716a96fe
sabbir347256 <srgamingzone71@gmail.com>
2024-06-29 18:41:11 +0600

    this is complete

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const cors = require('cors');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kd61vsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
-app.use(cors());
+app.use(cors({
+  origin : [
+    'https://blog-site-web-page-project.vercel.app',
+    'https://himu-s-lab.web.app',
+    'https://himu-s-lab.firebaseapp.com'
+  ]
+}));
 app.use(express.json());
 
 //himu_labs
@@ -14,7 +21,6 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kd61vsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
